711c4ff76d01effabd9393c97c8a3f7dde639c32
SahanSamarakoon <sahan.18@cse.mrt.ac.lk>
2021-09-27 20:17:46 +0530

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+
+function App() {
+
+  const [notes, setNote] = useState([]);
+
+  function addNote(inputNote) {
+    setNote(prevNotes => {
+      return [...prevNotes, inputNote];
+    });
+  };
+
+  function deleteNote(id) {
+    setNote(prevNotes => {
+      return prevNotes.filter((note, index) => { return index !== id; })
+    });
+  };
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return (<Note key={index} id={index} title={note.title} content={note.content} delete={deleteNote} />);
+      })
+      }
+      <Footer />
+    </div>
+  );
+}
+
+
+export default App;
